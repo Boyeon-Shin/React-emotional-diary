3fa243985c0fa0417c92880ec2a6e40736139616
pingmong <kny7024@naver.com>
2024-02-05 06:56:44 +0900

    feat: Editor 컴포넌트 구현

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import Button from "../components/Button";
-import Header from "../components/Header"
+import Header from "../components/Header";
+// import Editor from "../components/Editor";
 
 const Home = () => {
     return (
@@ -25,6 +26,18 @@ const Home = () => {
                 }
             />
         </div>
+
+        // 에디터 확인 코드
+        // <div>
+        //     <Editor
+        //         initData={{
+        //             date: new date().getTime(),
+        //             emotionId: 1,
+        //             content: "이전에 작성했던 일기",
+        //         }}
+        //         onSubmit={() => alert("작성 완료")}
+        //     />
+        // </div>
     );
 };
 export default Home;
